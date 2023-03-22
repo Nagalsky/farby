98702e0f882024eac4366df11d4536053fa34f64
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2023-03-22 10:45:09 +0100

    meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,11 @@ export default function Document() {
           name="description"
           content="✅ Vitajte na stránkach Obchodného centra L&Š Nitra - farby, železiarstvo, stavebniny, záhradné centrum, bývanie a drogéria otvorené 7 dní v týždni od 7:00 do 19:00!"
         />
+        <meta property="og:site_name" content="Farby" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.farby.sk/" />
+        <meta property="og:image" content="/header-logo.svg" />
+        <meta property="og:title" content="Farby" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta httpEquiv="Cache-control" content="public" />
       </Head>
